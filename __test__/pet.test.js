f3006f1ac52a41567a2c5fdb2bbd92796825c2f4
Ashley Daniels <ashdaniels25gmail.com>
2023-11-07 17:12:09 +0000

    Hunger & Fitness updated

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,35 +1,47 @@
-const Pet = require ('../src/pet');
+const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    test('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
+  test('returns an object', () => {
+    const pet = new Pet('Fido');
+    expect(pet).toBeInstanceOf(Object);
   });
 
-  describe('constructor', () => {
-    
-    test('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
-    });
+  test('sets the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
   });
 
-  describe('constructor', () => {
-
-    test('has an initial age of 0', () => {
-      const pet = new Pet ('Fido');
-
-      expect(pet.age).toEqual(0);
-    });
+  test('has an initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
   });
-  
-  describe('constructor', () => {
-    test('increments the age by 1', () => {
-      const pet = new Pet('Fido');
-  
-      pet.growUp();
-  
-      expect(pet.age).toEqual(1);
-    });
+
+  test('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
   });
+
+  test('has an initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+  });
+
+  test('increments the hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp2();
+    expect(pet.hunger).toEqual(5);
+  });
+
+test('has a fitness level of 10', () => {
+  const pet = new Pet('Fido');
+  expect(pet.fitness).toEqual(10);
+});
+
+
+test('Decreases fitness by 3', () => {
+  const pet = new Pet('Fido');
+  pet.growUp3();
+  expect(pet.fitness).toEqual(7);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -4,5 +4,13 @@ function Pet (name) {
     this.growUp = function () {
         this.age += 1;
     };
+    this.hunger = 0;
+    this.growUp2 = function () {
+        this.hunger += 5;
+    };
+    this.fitness = 10;
+    this.growUp3 = function () {
+        this.fitness -= 3;
+    };
     }
 module.exports = Pet; 
