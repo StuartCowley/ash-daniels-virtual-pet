2abc60a44c16e78f6e0069a17c534cbfc9215d05
Ashley Daniels <ashdaniels25gmail.com>
2023-11-07 16:06:38 +0000

    name function

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,6 +1,14 @@
 const Pet = require ('../src/pet');
 describe('constructor', () => {
-    it('returns an object', () => {
+    test('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+  });
+  describe('constructor', () => {
+    
+    test('sets the name property', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.name).toEqual('Fido');
     });
   });
