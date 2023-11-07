74add79c410bb8d9ac7cc97994922f98bbefac9c
Ashley Daniels <ashdaniels25gmail.com>
2023-11-07 16:36:33 +0000

    Added growUp

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,14 +1,35 @@
 const Pet = require ('../src/pet');
+
 describe('constructor', () => {
     test('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
   });
+
   describe('constructor', () => {
     
     test('sets the name property', () => {
       const pet = new Pet('Fido');
   
       expect(pet.name).toEqual('Fido');
+    });
+  });
+
+  describe('constructor', () => {
+
+    test('has an initial age of 0', () => {
+      const pet = new Pet ('Fido');
+
+      expect(pet.age).toEqual(0);
+    });
+  });
+  
+  describe('constructor', () => {
+    test('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
     });
   });
